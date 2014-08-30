8d4bae8d1c168477d043c01f9a0746b9928c9976
Dimitar Spiroski <dimitar.spiroski@gmail.com>
2014-08-30 17:03:56 +0200

    Don't load jquery if already loaded

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -1,4 +1,29 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script ></script>
+var jQueryScriptOutputted = false;
+function initJQuery() {
+    
+    //if the jQuery object isn't available
+    if (typeof(jQuery) == 'undefined') {
+    
+        if (! jQueryScriptOutputted) {
+            //only output the script once..
+            jQueryScriptOutputted = true;
+            
+            //output the script (load it from google api)
+            document.write("<scr" + "ipt type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></scr" + "ipt>");
+        }
+        setTimeout("initJQuery()", 50);
+    } else {
+                        
+        $(function() {  
+            // do anything that needs to be done on document.ready
+            // don't really need this dom ready thing if used in footer
+        });
+    }
+            
+}
+initJQuery();
+</script>
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
 <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/jqueryui-editable/css/jqueryui-editable.css" rel="stylesheet"/>
